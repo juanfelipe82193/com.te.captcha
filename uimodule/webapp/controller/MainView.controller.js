9b5941624616ae48c45fdee77cc21d21dbbff391
juanfelipe82193 <juan19_93@hotmail.com>
2021-07-20 19:39:53 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/te/captcha/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.te.captcha.controller.MainView", {});
+});
